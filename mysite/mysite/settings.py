15dfed780d72d4c89917d749f3d3415b38bbf7d6
Your Name <you@example.com>
2017-05-27 14:49:58 -0300

    adiciona endereço python

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bbb6m(gun-o-%hya^l=#-vz1g_@ee(dn_z)rd*+b#ji=_id)+%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tchela.pythonanywhere.com']
 
 
 # Application definition
